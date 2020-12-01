a5ef1610008c3af26bd30bb114aef1ecf2da2a88
Emanuel Posescu <emanuel.posescu@dacodasoft.ro>
2020-12-01 07:23:15 +0200

    Fix command lookup generation

--- src/api/commandBuilder.ts
+++ src/api/commandBuilder.ts
@@ -4,6 +4,8 @@ import { CommandType } from "../constant/CommandType";
 import { Command, CommandInterface } from "./Command";
 import * as commands from "./Commands";
 
+// TODO: index commands based on COMMAND_TYPE for faster lookup
+
 function getCommandClass(commandType: CommandType): CommandInterface | void {
   let commandTypeInt = commandType;
   if (typeof commandTypeInt == "string") {
@@ -11,19 +13,21 @@ function getCommandClass(commandType: CommandType): CommandInterface | void {
   }
   const classNames = Object.keys(commands);
   for (let i = 0; i < classNames.length; i++) {
-    const commandClass: CommandInterface = Reflect.get(commands, classNames[i]);
-    let cmdTypeInt = commandClass.COMMAND_TYPE;
-    if (typeof cmdTypeInt == 'string') {
-      cmdTypeInt = cmdTypeInt.charCodeAt(0);
-    }
-    if (cmdTypeInt == commandTypeInt) {
-      return commandClass;
+    if (classNames[i] != "UnknownCommand") {
+      const commandClass: CommandInterface = Reflect.get(commands, classNames[i]);
+      let cmdTypeInt = commandClass.COMMAND_TYPE;
+      if (typeof cmdTypeInt == 'string') {
+        cmdTypeInt = cmdTypeInt.charCodeAt(0);
+      }
+      if (cmdTypeInt == commandTypeInt) {
+        return commandClass;
+      }
     }
   }
 }
 
 export function commandFromData(data: Buffer): Command {
-  const commandType: CommandType = data.readInt8(0);
+  const commandType: CommandType = data.readUInt8(0);
   const commandClass = getCommandClass(commandType);
   if (commandClass) {
     return Reflect.construct(commandClass, [data]);
